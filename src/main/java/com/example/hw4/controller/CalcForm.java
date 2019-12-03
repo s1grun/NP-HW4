3951b0a862dd76159bd80fece98cfb96e770be6a
weng <1906338209@qq.com>
2019-12-03 16:13:11 +0100

    calculate

---
+++ src/main/java/com/example/hw4/controller/CalcForm.java
@@ -0,0 +1,13 @@
+package com.example.hw4.controller;
+
+/**
+ * Created by weng on 2019/12/3.
+ */
+public class CalcForm {
+    private float res;
+    private String currency1;
+    private String currency2;
+    private float num1;
+
+
+}
