3757bcec6bb72f74e166836388fbc21cc6c597f7
Sigrun Arna Sigurdardottir <sigruns@tempo.io>
2019-12-03 13:54:52 +0100

    repo, service, entity

--- src/main/java/com/example/hw4/application/CurrencyService.java
+++ src/main/java/com/example/hw4/application/CurrencyService.java
@@ -1,4 +1,21 @@
 package com.example.hw4.application;
 
+import com.example.hw4.domain.CurrencyEntity;
+import com.example.hw4.repository.CurrencyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@Service
 public class CurrencyService {
+    @Autowired
+    private CurrencyRepository currRepo;
+
+    public List<CurrencyEntity> getAllCurrencies() {
+        return currRepo.findAll();
+    }
+
 }
---
+++ src/main/java/com/example/hw4/repository/CurrencyRepository.java
@@ -0,0 +1,14 @@
+package com.example.hw4.repository;
+
+import com.example.hw4.domain.CurrencyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional(propagation = Propagation.MANDATORY)
+public interface CurrencyRepository extends JpaRepository<CurrencyEntity, List> {
+    //CurrencyEntity getListOfCurrency(List currencyList);
+}
